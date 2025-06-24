ba68a461bc702e8421aae16eb180f5dd3f012199
Victor Sandomirsky <victor.sandomirsky@gmail.com>
2025-06-24 22:21:40 +0200

    Refactored to separate game logic from game engine

---
+++ app.py
@@ -0,0 +1,70 @@
+import pygame
+import time
+
+RED = (255, 0, 0)
+
+class App:
+    def __init__(self, game, screen_width, screen_hight, background_color, fps):
+        self._running = True
+        #self._display_surf = None
+        self.size = self.weight, self.height = screen_width, screen_hight
+        self.game = game
+        self.screen_width = screen_width
+        self.screen_hight = screen_hight
+        self.background_color = background_color
+        self.mspf = (1/fps)*1000
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.SHOWN)
+        self._running = True
+        self._step = False
+        self._tick = False
+        self._pause = False
+        self._last_tick = 0
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+        if event.type == pygame.KEYDOWN:
+            if(event.key == pygame.K_SPACE):
+                self._pause = not self._pause
+            if(self._tick == True):
+                self.game.process_event(event)
+                self._tick = False
+
+    def on_loop(self):
+        self.game.tick()
+
+    def on_render(self):
+        self._display_surf.fill(pygame.Color(self.background_color))
+        self.game.render(self._display_surf)
+        pygame.display.flip()
+            
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+
+            ticks = pygame.time.get_ticks()
+            if((ticks - self._last_tick > self.mspf) and not self._pause and not self.game._game_over):
+                self._tick = True
+                self._last_tick = ticks
+                self.on_loop()
+                self.on_render()
+            else:
+                time.sleep(0.10)
+
+            if(self.game._game_over):
+                pygame.draw.line(self._display_surf, RED, (0,0), (self.screen_width-1, self.screen_hight-1), 25)
+                pygame.draw.line(self._display_surf, RED, (self.screen_width-1,0), (0, self.screen_hight-1), 25)
+                pygame.display.flip()
+                time.sleep(0.10)
+
+        self.on_cleanup()
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pygame
 import random
-import math
-import time
 from operator import add, sub
+import app
 
 # Colors
 WHITE = (255, 255, 255)
@@ -14,7 +13,7 @@ BLUE = (0, 0, 255)
 BACKGROUND_COLOR = (70, 130, 130)
 SNAKE_COLOR = (0, 255, 255)
 
-FPS = 4
+FPS = 8
 MSPF = (1/FPS)*1000
 
 EMPTY_SPACE = 0
@@ -31,12 +30,12 @@ DOWN = (0, 1)
 # Screen dimensions
 GRID_STEPS = 29
 GRID_WIDTH = 20
-GRID_HEIGHT = 20
+GRID_HIGHT = 20
 SCREEN_WIDTH = GRID_STEPS * GRID_WIDTH
-SCREEN_HEIGHT = GRID_STEPS * GRID_HEIGHT
+SCREEN_HIGHT = GRID_STEPS * GRID_HIGHT
 
-class Grid:
-    grid = [EMPTY_SPACE] * (GRID_WIDTH * GRID_HEIGHT)
+class GameLogic:
+    grid = [EMPTY_SPACE] * (GRID_WIDTH * GRID_HIGHT)
 
     snakeDir: tuple = LEFT
     snakePos = (0, 0)
@@ -47,7 +46,7 @@ class Grid:
     def __init__(self) -> None:
         self._game_over = False
         random.seed
-        self.snakePos = (random.randint(1, GRID_WIDTH-1), random.randint(1, GRID_HEIGHT-1))
+        self.snakePos = (random.randint(1, GRID_WIDTH-1), random.randint(1, GRID_HIGHT-1))
         (taili, tailj) = tuple(map(sub, self.snakePos, self.snakeDir))
         self.snakeTail.append((taili,tailj))
 
@@ -58,7 +57,7 @@ class Grid:
         self.new_apple()
 
     def new_apple(self):
-        self.applePos = (applei, applej) = (random.randint(1, GRID_WIDTH-1), random.randint(1, GRID_HEIGHT-1))
+        self.applePos = (applei, applej) = (random.randint(1, GRID_WIDTH-1), random.randint(1, GRID_HIGHT-1))
         self.set_item(applei, applej, APPLE)
 
     def get_item(self, i, j):
@@ -68,7 +67,7 @@ class Grid:
         self.grid[j*GRID_WIDTH + i] = item
     
     def clear_grid(self):
-        self.grid = [EMPTY_SPACE] * (GRID_WIDTH * GRID_HEIGHT)
+        self.grid = [EMPTY_SPACE] * (GRID_WIDTH * GRID_HIGHT)
 
     def update_grid(self):
         (i, j) = self.snakePos
@@ -89,7 +88,7 @@ class Grid:
             nextj = GRID_WIDTH-1
         if(nexti > GRID_WIDTH-1):
             nexti = 0
-        if(nextj > GRID_HEIGHT-1):
+        if(nextj > GRID_HIGHT-1):
             nextj = 0
 
         if(self.get_item(nexti, nextj) == EMPTY_SPACE):
@@ -106,120 +105,49 @@ class Grid:
         self.clear_grid()
         self.update_grid()
 
-class App:
-    grid = Grid()
-
-    def __init__(self):
-        self._running = True
-        #self._display_surf = None
-        self.size = self.weight, self.height = SCREEN_WIDTH, SCREEN_HEIGHT
-
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.SHOWN)
-        self._running = True
-        self._step = False
-        self._tick = False
-        self._pause = False
-        self._last_tick = 0
-
-        
-
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
-        if event.type == pygame.KEYDOWN:
-            if(event.key == pygame.K_SPACE):
-                self._pause = not self._pause
-            if(self._tick == True):
-                if(event.key == pygame.K_UP and self.grid.snakeDir != DOWN):
-                    self.grid.snakeDir = UP
-                    pygame.display.set_caption("UP")
-                if(event.key == pygame.K_DOWN and self.grid.snakeDir != UP):
-                    self.grid.snakeDir = DOWN
-                    pygame.display.set_caption("DOWN")
-                if(event.key == pygame.K_LEFT and self.grid.snakeDir != RIGHT):
-                    self.grid.snakeDir = LEFT
-                    pygame.display.set_caption("LEFT")
-                if(event.key == pygame.K_RIGHT and self.grid.snakeDir != LEFT):
-                    self.grid.snakeDir = RIGHT
-                    pygame.display.set_caption("RIGHT")
-                self._tick = False
-
-    def on_loop(self):
-        self.grid.tick()
-
-    def on_render(self):
-        self._display_surf.fill(pygame.Color(BACKGROUND_COLOR))
-        self.draw_grid()
-        pygame.display.flip()
-
-    def draw_shape(self, i, j):
-        if(self.grid.get_item(i,j) == SNAKE_HEAD):
+    def draw_shape(self, surface, i, j):
+        if(self.get_item(i,j) == SNAKE_HEAD):
             center = (i*GRID_STEPS + GRID_STEPS/2, j*GRID_STEPS + GRID_STEPS/2)
-            pygame.draw.circle(self._display_surf,
+            pygame.draw.circle(surface,
                             SNAKE_COLOR,
                             center,
                             GRID_STEPS/2,)
             
-        elif(self.grid.get_item(i, j) == SNAKE_TAIL):
-            pygame.draw.rect(self._display_surf, pygame.Color(255,255,255),
+        elif(self.get_item(i, j) == SNAKE_TAIL):
+            pygame.draw.rect(surface, pygame.Color(255,255,255),
                                     pygame.Rect(i*GRID_STEPS,
                                              j*GRID_STEPS,
                                              GRID_STEPS,
                                              GRID_STEPS),
                                     0)
-        elif(self.grid.get_item(i,j) == APPLE):
+        elif(self.get_item(i,j) == APPLE):
             center = (i*GRID_STEPS + GRID_STEPS/2, j*GRID_STEPS + GRID_STEPS/2)
-            pygame.draw.circle(self._display_surf,
+            pygame.draw.circle(surface,
                             RED,
                             center,
                             GRID_STEPS/2,)
-        elif(self.grid.get_item(i, j) == EMPTY_SPACE):
-            pygame.draw.rect(self._display_surf, pygame.Color(0,200,150),
+        elif(self.get_item(i, j) == EMPTY_SPACE):
+            pygame.draw.rect(surface, pygame.Color(0,200,150),
                                     pygame.Rect(i*GRID_STEPS,
                                              j*GRID_STEPS,
                                              GRID_STEPS,
                                              GRID_STEPS),
                                              1)
-            
-    def draw_grid(self):
-        for j in range(0, GRID_HEIGHT):
+    def render(self, surface):
+        for j in range(0, GRID_HIGHT):
             for i in range(0, GRID_WIDTH):
-                self.draw_shape(i, j)
-                
-    
+                self.draw_shape(surface, i, j)
 
-    def on_cleanup(self):
-        pygame.quit()
-
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
- 
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-
-            ticks = pygame.time.get_ticks()
-            if((ticks - self._last_tick > MSPF) and not self._pause and not self.grid._game_over):
-                self._tick = True
-                self._last_tick = ticks
-                self.on_loop()
-                self.on_render()
-            else:
-                time.sleep(0.10)
-
-            if(self.grid._game_over):
-                pygame.draw.line(self._display_surf, RED, (0,0), (SCREEN_WIDTH-1, SCREEN_HEIGHT-1), 25)
-                pygame.draw.line(self._display_surf, RED, (SCREEN_WIDTH-1,0), (0, SCREEN_HEIGHT-1), 25)
-                pygame.display.flip()
-                time.sleep(0.10)
-
-
-        self.on_cleanup()
+    def process_event(self, event):
+        if(event.key == pygame.K_UP and self.snakeDir != DOWN):
+            self.snakeDir = UP
+        if(event.key == pygame.K_DOWN and self.snakeDir != UP):
+            self.snakeDir = DOWN
+        if(event.key == pygame.K_LEFT and self.snakeDir != RIGHT):
+            self.snakeDir = LEFT
+        if(event.key == pygame.K_RIGHT and self.snakeDir != LEFT):
+            self.snakeDir = RIGHT
         
- 
 if __name__ == "__main__" :
-    theApp = App()
+    theApp = app.App(GameLogic(), SCREEN_WIDTH, SCREEN_HIGHT, BACKGROUND_COLOR, FPS)
     theApp.on_execute()
